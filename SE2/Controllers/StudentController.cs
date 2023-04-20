12291dd8a062585861ca8bea031d3578529f4f4f
said-ahmd <said.ahmd.cs@gmail.com>
2023-04-20 16:56:45 +0200

    task

---
+++ SE2/Controllers/StudentController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SE2.Models;
+
+namespace SE2.Controllers
+{
+    public class StudentController : Controller
+    {
+        public IActionResult Index()
+        {
+            List<Student> students = new List<Student>
+            {
+                new Student{Id=1 ,Name="sadi",Department="cs" },
+                new Student{Id=2 ,Name="Ahmd" ,Department="it"},
+                new Student{Id=3 ,Name="khaled" ,Department="is"},
+                new Student{Id=4 ,Name="mhmd" ,Department="cs"},
+                new Student{Id=5 ,Name = "Amgad" ,Department="is"}
+            };
+            return View(students);
+        }
+    }
+}
